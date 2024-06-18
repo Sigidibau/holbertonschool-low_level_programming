5d912ebd732a6566f993ccd2a477d28187b966ab
sigidibau <adriansantiago33@gail.com>
2024-06-18 19:05:30 -0400

    all

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d  and is less than 6 and not 0\n", n, lst);
+		printf("Last digit of %d is and is %d less than 6 and not 0\n", n, lst);
 	}
 	return (0);
 }
