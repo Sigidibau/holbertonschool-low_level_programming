a8edf3a5b6b82bca6697bd8311d03c105cb7f079
sigidibau <adriansantiago33@gail.com>
2024-06-20 13:48:28 -0400

    all

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-* owned by Bwave ICT
-*Return: void
-*/
+ * print_numbers - Print the numbers from 0 up to 9
+ * Return: The numbers from 0 up to 9
+ */
 
 void print_numbers(void)
-{
-	char c;
 
-	for (c = '0'; c <= '9'; c++)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
 	{
-	_putchar(c);
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
