9f115424b9a5fd5929f4ccdd2751bcc08cdbbea6
sigidibau <9571@holbertonstudents.com>
2024-06-27 17:14:59 -0400

    memeset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
  *@s: pointer to me area to fill
  *@n: nuber of byte to fill 
  *@b: constant
- *Return: pointer to the memory area s
+ *Return: pointer to the memory area @s
  */
 
 char *_memset(char *s, char b, unsigned int n)
