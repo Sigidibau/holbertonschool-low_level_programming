0c9eb6a33ee19b6eb8440dc89b1190150e28ad4c
sigidibau <adriansantiago33@gail.com>
2024-06-28 12:26:40 -0400

    memeset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memset - fill mem with a constant byte
+ *@s: pointer to me area to fill
+ *@n: nuber of byte to fill
+ *@b: constant
+ *Return: pointer to the memory area @s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int m = 0;
+
+	for (m = 0; m < n; m++)
+	{
+		s[m] = b;
+	}
+	return (s);
+}
