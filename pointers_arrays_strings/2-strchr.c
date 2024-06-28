fb64190b9b73d14c3b6db568c6f2f9a2182859d8
sigidibau <adriansantiago33@gail.com>
2024-06-28 12:58:17 -0400

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,19 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ *_strchr - locates a char in a string.
+ *@s: string
+ *@c: char
+ *return: pointer to the first char c in the string
  */
 
 char *_strchr(char *s, char c)
 {
-	int a = 0;
-
-	while (a < c)
-	
-		if (a < c)
-			c++;
-	return (c);
-
-	else
-
+	while (*s != '\0')
+	{
+		if (*s ==  c)
+			return (s);
+		s++;
+	}
 		return ('\0');
 }
