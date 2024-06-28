11122400096de1da9a8001267b6ce916ef67c492
sigidibau <adriansantiago33@gail.com>
2024-06-28 12:33:24 -0400

    all

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	while (a < c)
+	
+		if (a < c)
+			c++;
+	return (c);
+
+	else
+
+		return ('\0');
+}
