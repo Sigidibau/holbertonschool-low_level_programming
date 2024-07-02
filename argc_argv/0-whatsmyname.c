a22c2adac6e9533a1e7b7e85cd7d928abc24f695
sigidibau <9571@holbertonstudents.com>
2024-07-02 13:04:56 -0400

    whatsmyname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,17 +2,15 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ *main - Write a program that prints its name, followed by a new line.
+ *@argc: count
+ *@argv: vector
+ *return: return 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)), char *argv[])
 {
-	(void)argc;
-	
+	(0)argc;
 	printf("%s\n", argv[0]);
-	
-	return (0);	
-}	
+	return (0);
+}
