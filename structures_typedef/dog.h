a4121fc4dd2cf5af108ef8a6e9bb06558a575b72
sigidibau <adriansantiago33@gail.com>
2024-07-08 12:51:04 -0400

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,17 @@
+#ifndef D
+#define D
+
 /**
  *struct dog - description of the dog
  *@name: name of the dog
- *@age - age of the dog
- *@owner - owner of the dog
+ *@age: age of the dog
+ *@owner:  owner of the dog
  */
 
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
+#endif
