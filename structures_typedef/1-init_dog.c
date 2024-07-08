c7319ad37e66bee93e08292871ca82e221a5f224
sigidibau <adriansantiago33@gail.com>
2024-07-08 14:03:07 -0400

    init

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ *init_dog - initialize variable
+ *@d: point of start
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *return - null
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
