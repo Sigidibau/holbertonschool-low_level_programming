b640960b4e8e507c670662568a2798837755c582
sigidibau <adriansantiago33@gail.com>
2024-07-08 12:26:34 -0400

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,13 @@
-#include "dog.h"
-
 /**
- *
- *
- *
- *
- *
+ *struct dog - description of the dog
+ *@name: name of the dog
+ *@age - age of the dog
+ *@owner - owner of the dog
  */
 
 struct dog
 {
-  char* name;
+  char *name;
   float age;
-  char* owner;
-}dog;
+  char *owner;
+};
