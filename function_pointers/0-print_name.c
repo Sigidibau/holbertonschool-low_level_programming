d4771c9bf06d905e62c139464aafb59c8d748811
sigidibau <9571@holbertonstudents.com>
2024-07-09 10:43:05 -0400

    name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: the function to send the name to
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
