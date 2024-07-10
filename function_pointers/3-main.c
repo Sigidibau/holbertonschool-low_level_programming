786b2dc7bd9cd233ef1418cdf126245e713a7669
sigidibau <adriansantiago33@gail.com>
2024-07-10 11:13:04 -0400

    3main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,15 +21,15 @@ int main(int argc, char *argv[])
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	p = get_op_func(argv[2]);
 
+	p = get_op_func(argv[2]);
 	if (p == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
 	c = (*p)(a, b);
+
 	printf("%d\n", c);
 	return (0);
 }
