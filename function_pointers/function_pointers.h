135e4420f13afa73aeb089686d0ce741836cffe0
sigidibau <adriansantiago33@gail.com>
2024-07-10 10:26:10 -0400

    header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#ifndef F_P
+#define F_P
 
 int _putchar(char c);
 
@@ -10,3 +12,4 @@ int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
